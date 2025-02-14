c8cef89adfbfb36a56c43106c23572de1e6ebf8f
keith <mwanazk@gmail.com>
2025-02-14 01:03:30 +0000

    allowed hosts

--- backend/settingsprod.py
+++ backend/settingsprod.py
@@ -26,7 +26,10 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1",'api.koiinx.com']
+ALLOWED_HOSTS = [
+    '138.68.183.251',
+    'koiinx.com',
+    'www.koiinx.com',]
 
 
 # Application definition
