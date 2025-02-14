062dee7f36f423f7e281330367d17943ee8fd173
keith <mwanazk@gmail.com>
2025-02-14 14:13:14 +0000

    allowedhosts and coinfig

--- backend/settings.py
+++ backend/settings.py
@@ -23,12 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6q7d#@4k$!cmx#-7p$-%%qpe-!m&)imc)v5^11v+^x4tl_q703'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
